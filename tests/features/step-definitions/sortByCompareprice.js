9fba37a1139597c930729cc3d4a69b643381395c
AccidentByDragon <karl17loe@hotmail.com>
2024-11-13 11:39:34 +0100

    v1 compare Price test
    Gjort en första skiss av Price tests

--- tests/features/step-definitions/sortByCompareprice.js
+++ tests/features/step-definitions/sortByCompareprice.js
@@ -2,6 +2,19 @@
 import { Given, When, Then } from '@cucumber/cucumber';
 import { expect } from 'chai';
 
-Then('the products sorted in order from {string} {string}', async function(a, b){
+Then('the products sorted in order from {string} {string}', async function (expectedOrder, valueToTest) {
   // TODO: implement step
+  let data = this.json.children.children.valueToTest;
+  for (let i = 0; i < data.length - 1; i++) {
+    if (expectedOrder === "highest") {
+      if (data[i].valueToTest > data[i + 1].valueToTest) {
+        //return false;  // If an item is greater than the next one, it's not sorted
+      }
+    }
+    if (expectedOrder === "lowest") {
+      if (data[i].valueToTest < data[i + 1].valueToTest) {
+        //return false;  // If an item is less than the next one, it's not sorted
+      }
+    }
+  }
 });
